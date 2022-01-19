22f13ba8db9fcf46932b7ed012459855025709b2
Sh-Jil <65016071+Sh-Jil@users.noreply.github.com>
2022-01-20 00:14:02 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -47,10 +47,7 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
-    ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Group 🛠', url='https://t.me/Cinemapranthanmaar')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
